79b60b9dbc4104a5b5397b8e2a971a8efbab9e42
seba-1511 <seba-1511@hotmail.com>
2017-10-10 03:16:46 -0700

    Updated rot3d, setup.py with proper info

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 from setuptools import (
     setup as install,
     find_packages,
 )
 
-VERSION = '0.1.0'
-
-setup(
-    name='plotify',
-)
+VERSION = '0.1.1'
 
 install(
     name='plotify',
@@ -20,11 +15,8 @@ install(
     author='Seb Arnold',
     author_email='smr.arnold@gmail.com',
     url='http://www.seba1511.com',
-    download_url='https://github.com/seba-1511/plotify/archive/0.1.0.zip',
+    download_url='https://github.com/seba-1511/plotify/archive/0.1.1.zip',
     license='License :: OSI Approved :: Apache Software License',
     packages=find_packages(exclude=["tests"]),
-    classifiers=[
-        'Tools',
-        'Productivity',
-    ]
+    classifiers=[]
 )
